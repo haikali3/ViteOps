4c937f83086f718aa82b968d2e5735736d8ac045
MUHAMMAD HAIKAL BIN MOHD TAHAR <muhammadhaikal.binmohdtahar@etiqa.com.my>
2025-01-03 09:31:08 +0800

    remove unused S3 bucket configuration from Terraform

--- terraform/s3-bucket.tf
+++
@@ -1,8 +0,0 @@
-# # Data blocks for existing S3 buckets
-# resource "aws_s3_bucket" "input_bucket" {
-#   bucket = "s3-viteops-input"
-# }
-
-# resource "aws_s3_bucket" "output_bucket" {
-#   bucket = "s3-viteops-output"
-# }
